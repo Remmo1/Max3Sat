8553f2a5cb60ff5641fd255a185a6acc04d4ae4b
Remigiusz Pisarski <Remigiusz.pisarski@onet.pl>
2022-01-19 19:20:38 +0100

    Final version, correct the problem with -0.

--- MiniProjekt/CMax3SatProblem.cpp
+++ MiniProjekt/CMax3SatProblem.cpp
@@ -24,6 +24,9 @@ std::vector<Clause*> CMax3SatProblem::load(int amountOfClauses, std::string file
 	std::vector<std::string> outputArray;
 	std::string val;
 
+	// sta³a potrzebna do nieprzyjemnego -0
+	int MINUSZERO = 123456;
+
 	// odczytujemy dane
 	int i = 0;
 	while (i < amountOfClauses) {
@@ -35,12 +38,13 @@ std::vector<Clause*> CMax3SatProblem::load(int amountOfClauses, std::string file
 		while (std::getline(streamData, val, separator)) 
 			outputArray.push_back(val);
 		
+		// sprawdzenie nieprzyjemnego przypadku -0
+		int st = outputArray[1] == "-0" ? MINUSZERO : stoi(outputArray[1]);
+		int nd = outputArray[3] == "-0" ? MINUSZERO : stoi(outputArray[3]);
+		int rd = outputArray[5] == "-0" ? MINUSZERO : stoi(outputArray[5]);
+
 		// nowa klauzla wrzucana do wektora
-		Clause * read = new Clause(
-			stoi(outputArray[1]),
-			stoi(outputArray[3]),
-			stoi(outputArray[5])
-		);
+		Clause * read = new Clause(st, nd, rd);
 
 		clauses.push_back(read);
 		outputArray.clear();
@@ -56,7 +60,13 @@ std::vector<Clause*> CMax3SatProblem::load(int amountOfClauses, std::string file
 
 
 int CMax3SatProblem::compute(bool* solution, int amountOfClauses, std::vector<Clause*>& clauses) {
+
+	// licznik rozwi¹zania
 	int counter = 0;
+
+	// sta³a potrzebna dla -0
+	int MINUSZERO = 123456;
+
 	for (int i = 0; i < amountOfClauses; i++) {
 
 		/*
@@ -67,9 +77,14 @@ int CMax3SatProblem::compute(bool* solution, int amountOfClauses, std::vector<Cl
 		* przechodzimy dalej u¿ywaj¹c continue
 		*/
 
+		// wartoœæ pierwsza w klauzuli
 		int nr = clauses.at(i)->getSt();
 
-		if (nr >= 0 && solution[nr]) {
+		if (nr == MINUSZERO && solution[0] == 0) {
+			counter++;
+			continue;
+		}
+		else if (nr >= 0 && nr != MINUSZERO && solution[nr]) {
 			counter++;
 			continue;
 		}
@@ -80,9 +95,15 @@ int CMax3SatProblem::compute(bool* solution, int amountOfClauses, std::vector<Cl
 			}
 		}
 
+
+		// wartoœæ druga w klauzuli
 		int nr2 = clauses.at(i)->getNd();
 
-		if (nr2 >= 0 && solution[nr2]) {
+		if (nr2 == MINUSZERO && solution[0] == 0) {
+			counter++;
+			continue;
+		}
+		else if (nr2 >= 0 && nr2 != MINUSZERO && solution[nr2]) {
 			counter++;
 			continue;
 		}
@@ -93,9 +114,13 @@ int CMax3SatProblem::compute(bool* solution, int amountOfClauses, std::vector<Cl
 			}
 		}
 
+
+		// wartoœæ trzecia w klauzuli
 		int nr3 = clauses.at(i)->getRd();
 
-		if (nr3 >= 0 && solution[nr3])
+		if (nr3 == MINUSZERO && solution[0] == 0)
+			counter++;
+		else if (nr3 >= 0 && nr3 != MINUSZERO && solution[nr3]) 
 			counter++;
 		else {
 			if (nr3 < 0 && !solution[std::abs(nr3)])
